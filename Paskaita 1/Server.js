6dcbeb0a72252115c3a59dd3668a5b942863c0bf
PLIUSAS <Laurynas1297@gmail.com>
2023-09-12 19:49:31 +0300

    Paskaita 1

--- Paskaita 1/Server.js
+++ Paskaita 1/Server.js
@@ -1,1 +1,23 @@
-console.log("Labas is node");
+// npm init -y  - sukuria node aplikacija
+// npm run - paleidzia skripta is packege.json
+// npm install [package pavadinimas] - suinstaliuoja npm package
+// npm install --save-dev [package pavadinimas] - suinstaliuoja npm package kaip dev dependency
+// npm install -g [package pavadinimas] - suinstaliuoja npm package globaliai
+// nodemon [failo pavadinimas.js] - paleidzia aplikacija
+// npm install - atsiuncia visus package is package.json
+// ctrl + c - sustabdo terminalo veikima
+
+import express from "express";
+
+const app = express();
+
+app.get("/test", (req, res) => {
+  res.json({
+    message: "Hello world!",
+  });
+});
+// http://localhost:3000/test
+
+app.listen(3000, () => {
+  console.log("Server running on PORT 3000");
+});
