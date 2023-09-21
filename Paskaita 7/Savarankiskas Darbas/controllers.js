1a477874535670eadc60f6f93743ddac82d44937
PLIUSAS <Laurynas1297@gmail.com>
2023-09-21 21:50:17 +0300

    Paskaita 7

--- Paskaita 7/Savarankiskas Darbas/controllers.js
+++ Paskaita 7/Savarankiskas Darbas/controllers.js
@@ -17,5 +17,22 @@ export async function createpeople(req, res) {
 }
 
 export async function getpeople(req, res) {
-  res.json(people);
+  const { name, lastname, age } = req.query;
+  try {
+    const query = {};
+    if (name) {
+      query.name = name;
+    }
+    if (lastname) {
+      query.lastname = lastname;
+    }
+    if (age) {
+      query.age = age;
+    }
+
+    const peoples = await people.find(query, { __v: 0 });
+    res.json(peoples);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
 }
